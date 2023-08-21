a9397f14fe235c191aded219ba92420f86415e12
Brunoaqu <bruno.gl.aquino@gmail.com>
2023-08-21 11:08:17 -0300

    chore(gh-pages): updated docs

--- src/plugins/topbar/logo.jsx
+++ src/plugins/topbar/logo.jsx
@@ -1,8 +1,7 @@
 import React from "react"
-// import SwaggerUILogo from "./logo_small.svg"
 
 export const Logo = () => (
-  <img height="92" src="https://pixlog.com.br/wp-content/uploads/elementor/thumbs/pixlog-branca-transparente-prmhinxyayggqs10uot56oq3fnua4dty6fqppflfo8.png" alt="Swagger UI"/>
+  <img height="92" src="https://pixlog.com.br/wp-content/uploads/elementor/thumbs/pixlog-branca-transparente-prmhinxyayggqs10uot56oq3fnua4dty6fqppflfo8.png" alt="Pixlog TECH LTDA"/>
 )
 
 export default Logo
